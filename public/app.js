3a57f6e1fdbcdc81392f2623c6d77107a0f0667d
r.kugot <stream13k@gmail.com>
2017-06-10 15:27:12 +0300

    Edit app.js

--- public/app.js
+++ public/app.js
@@ -10,10 +10,10 @@ function something()
 
 }
 
-function () {
-  // инициализировать все элементы на страницы, имеющих атрибут data-toggle="tooltip", как компоненты tooltip
-  [data-toggle="tooltip"].tooltip()
-}
+// function () {
+//   // инициализировать все элементы на страницы, имеющих атрибут data-toggle="tooltip", как компоненты tooltip
+//   [data-toggle="tooltip"].tooltip()
+// }
 
 // <!-- Скрипт для инициализации элементов на странице, имеющих атрибут data-toggle="tooltip" -->
 // <script>
