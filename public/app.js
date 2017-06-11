a8118f5d092a2ef9ac75d31b8d533813362a238c
r.kugot <stream13k@gmail.com>
2017-06-11 19:19:18 +0300

    Add cart_get_number_of_items

--- public/app.js
+++ public/app.js
@@ -10,15 +10,34 @@ function something()
 
 }
 
-function add_to_cart(id) //change to button plus
+function add_to_cart(id)
 {
 	var key = 'product_' + id;
 	var x = window.localStorage.getItem(key); // create unic key for product in localStorage
 	x = x*1 + 1;
 	window.localStorage.setItem(key, x);
+	alert('Items in your cart: '+cart_get_number_of_items());
 }
 
-//add function remove_from_cart - button minus
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++) //аналог for each in ruby
+	{
+		var key = window.localStorage.key(i); //получаем ключ 
+		var value = window.localStorage.getItem(key); //получаем значение по ключу
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
+}
+
+//add function remove_from_cart, add plus and minus buttons
 
 $(function () {
   // инициализировать все элементы на страницы, имеющих атрибут data-toggle="tooltip", как компоненты tooltip
