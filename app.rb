9b80c905085db4f42ecd3d3e979b72a5ac04c2fe
r.kugot <stream13k@gmail.com>
2017-06-12 12:54:11 +0300

    Add parse_orders_input, table view to cart.erb

--- app.rb
+++ app.rb
@@ -21,9 +21,22 @@ end
 post '/cart' do
 	@orders = params[:ordersinput]
 	@items = parse_orders_input(@orders)
+
+	@items.each do |item|
+		item[0] = Product.find(item[0])
+	end
+	
 	erb :cart
 end
 
 def parse_orders_input(orders)
-	orders.split(',')
+	s = orders.split(',')
+	arr = []
+	s.each do |x|
+		s1 = x.split('=')
+		s2 = s1[0].split('_')
+		arr2 = [s2[1],s1[1]]
+		arr << arr2
+	end
+	return arr
 end
