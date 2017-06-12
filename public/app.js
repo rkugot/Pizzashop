891c116d3ac6d05ae0856d5ede37ed1c6b7882a5
r.kugot <stream13k@gmail.com>
2017-06-12 21:42:49 +0300

    Delete useless js function

--- public/app.js
+++ public/app.js
@@ -1,15 +1,3 @@
-function something()
-{
-	var x = window.localStorage.getItem('aaa'); // x = hh['aaa']
-
-	x = x*1 + 1; // x = x + 1
-
-	window.localStorage.setItem('aaa', x); // hh['aaa'] = x
-
-	alert(x);
-
-}
-
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
